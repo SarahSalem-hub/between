fcb298355b46bc81f526ff342bdc2e76eb2308d1
sarahsalem-hub <sarsarine01@gmail.com>
2022-02-14 15:50:04 +0300

    Running a flutterFlow.io template in "first.dart",with some changes in other files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:between/first.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( const MaterialApp(home:  HomePageWidget(),));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key key,  this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
